c1197722f806f346c0b2c43ddfa07ac30d921889
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-04-28 18:33:42 -0400

    chore: create map markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,3 @@
-import { User } from './User';
-import { Company } from './Company';
-
 interface Mappable {
   location: {
     lat: number;
@@ -25,12 +22,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi!',
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
