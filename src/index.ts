445c52cf24bb99d3252a542b7412ab49b94de698
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-04-28 09:50:14 -0400

    chore: add Google maps integration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
 
@@ -6,3 +7,5 @@ console.log(user);
 
 const company = new Company();
 console.log(company);
+
+google.maps;
